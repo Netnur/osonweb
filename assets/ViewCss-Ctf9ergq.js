f3f1af871acce691224a290620f0393a26866702
Ziyo <developerziyo@gmail.com>
2024-08-01 16:56:48 +0500

    Updates

---
+++ assets/ViewCss-Ctf9ergq.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./index-BJwUJMKO.js";const t={};function e(r,n,o,c,_,a){return null}const f=s(t,[["render",e]]);export{f as default};
---
+++ assets/ViewHtml-BcVzZoIG.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o}from"./index-BJwUJMKO.js";const t={};function c(s,n,r,_,p,l){return o(),a("h1",null,"salom")}const f=e(t,[["render",c],["__scopeId","data-v-a25ab4ad"]]);export{f as default};
