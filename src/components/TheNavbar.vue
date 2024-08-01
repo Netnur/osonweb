9ae211f744e1758bee50d4f3f1c740ab106ec2fb
Ziyo <developerziyo@gmail.com>
2024-08-01 16:08:02 +0500

    adding

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <h1>osonweb</h1>
+      </a>
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">HTML</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CSS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">JAVASCRIPT</a>
+          </li>
+        </ul>
+        <form class="d-flex input-group w-auto">
+          <input type="search" class="form-control" placeholder="Qidirish" aria-label="Search" />
+          <button class="btn btn-outline-primary" type="button">
+            <i class="fas fa-search"></i>
+          </button>
+        </form>
+        
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
